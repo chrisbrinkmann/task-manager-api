b148c55f6407ae1e2b92a567e7329b5a5d65a429
chrisbrinkmann <brinkmcd@gmail.com>
2019-09-15 15:12:47 -0400

    added REST API CRUD endpoints

--- src/index.js
+++ src/index.js
@@ -3,7 +3,33 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+// create a new task
+app.post('/tasks', (req, res) => {
+  res.send('Great success!')
+})
 
-app.listen(PORT, () => {
+// retrieve all tasks
+app.get('/tasks', (req, res) => {
+  res.send('Great success!')
+})
+
+// retrieve a single task
+app.get('/tasks/:id', (req, res) => {
+  res.send('Great success!')
+})
+
+// update a task
+app.patch('/tasks/:id', (req, res) => {
+  res.send('Great success!')
+})
+
+//delete a task
+app.delete('/tasks/:id', (req, res) => {
+  res.send('Great success!')
+})
+
+
+
+app.listen(port, () => {
   console.log('Server is listening on port: ' + port)
 })
