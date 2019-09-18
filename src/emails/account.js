176737667bbde0d215b11cc44aa5ba3b6df3eb55
chrisbrinkmann <brinkmcd@gmail.com>
2019-09-18 19:57:41 -0400

    add dotenv config to npm dev script

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,5 +1,4 @@
 const sgMail = require('@sendgrid/mail')
-require('dotenv').config()
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(express.json()) // auto parse req json to object
 app.use(userRouter) // register routers with app
