1fa4ba6f3debea2f8c80247371f1a5b2a0dcaec3
chrisbrinkmann <brinkmcd@gmail.com>
2019-09-18 17:52:41 -0400

    added welcome and goodbye email functions to accoun.js

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,3 +1,27 @@
 const sgMail = require('@sendgrid/mail')
+require('dotenv').config()
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'task-app@fakenews.com',
+    subject: 'Thank you for joining our cult!',
+    text: `Welcome to the cult, ${name}.`
+  })
+}
+
+const sendGoodbyeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'task-app@fakenews.com',
+    subject: `You think you're better than us?`,
+    text: `We know where you live, ${name}.`
+  })
+}
+
+module.exports = {
+  sendWelcomeEmail,
+  sendGoodbyeEmail
+}
