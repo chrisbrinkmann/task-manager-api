57390812ae68be63d284eeab1add19d6096abda6
chrisbrinkmann <brinkmcd@gmail.com>
2019-09-18 20:03:38 -0400

    moved database url to env var

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // connect to db instance; create new if not exists
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useCreateIndex: true, // creates indexes for data for convenience
   useUnifiedTopology: true, // dunno, put it to remove warning
