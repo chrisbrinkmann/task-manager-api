be10c6de345252ae2016d0f323e005fe7ad8faad
chrisbrinkmann <brinkmcd@gmail.com>
2019-09-19 22:50:36 -0400

    added fixtures profile pic to tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -24,7 +24,7 @@ const userOne = {
 // runs before each test
 beforeEach(async () => {
   await User.deleteMany() // clear DB users collection
-  await new User(userOne).save() // insert sample user
+  await new User(userOne).save() // insert sample user in DB collection
 })
 
 /**
@@ -34,16 +34,16 @@ beforeEach(async () => {
 // Create new user
 test('Should signup a new user', async () => {
   const response = await request(app)
-    .post('/users')
+    .post('/users') // insert to DB 
     .send({
       name: 'Andrew',
       email: 'andrew@example.com',
       password: 'MyPass777!'
     })
-    .expect(201)
+    .expect(201) // Assert res.status
   
   // Assert that the DB was changed correctly
-  const user = await User.findById(response.body.user._id)
+  const user = await User.findById(response.body.user._id) // DB query
   expect(user).not.toBeNull()
 
   // Assertions about the response
@@ -62,14 +62,14 @@ test('Should signup a new user', async () => {
 // Login existing user
 test('Should login existing user', async () => {
   const response = await request(app)
-    .post('/users/login')
+    .post('/users/login') // update to DB (user.tokens)
     .send({
       email: userOne.email,
       password: userOne.password
     })
-    .expect(200)
+    .expect(200) // Assert res.status
   
-  const user = await User.findById(response.body.user._id)
+  const user = await User.findById(response.body.user._id) // DB query
 
   // Assert that the response token matches the user's next token
   expect(user.tokens[1].token).toBe(response.body.token)
@@ -78,40 +78,40 @@ test('Should login existing user', async () => {
 // Login user: nonexistant email
 test('Should not login nonexistent user', async () => {
   await request(app)
-    .post('/users/login')
+    .post('/users/login') // update to DB (user.tokens)
     .send({
       email: 'invalidemail@example.com',
       password: 'asdfqwer4321'
     })
-    .expect(400)
+    .expect(400) // Assert res.status
 })
 
 // Get user profile
 test('Should get profile for user', async () => {
   await request(app)
-    .get('/users/me')
+    .get('/users/me') // select from DB
     .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
     .send()
-    .expect(200)
+    .expect(200) // Assert res.status
 })
 
 // Get user profile, no auth token
 test('Should not get profile for unauthenticated user', async () => {
   await request(app)
-    .get('/users/me')
+    .get('/users/me') // select from DB
     .send()
-    .expect(401)
+    .expect(401) // Assert res.status
 })
 
 // Delete user account
 test('Should delete account for user', async () => {
   await request(app)
-    .delete('/users/me')
+    .delete('/users/me') // delete from DB
     .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
     .send()
-    .expect(200)
+    .expect(200) // Assert res.status
   
-  const user = await User.findById(userOneId)
+  const user = await User.findById(userOneId) // DB query 
 
   // Assert userOne no longer in DB after delete
   expect(user).toBeNull()
@@ -120,7 +120,7 @@ test('Should delete account for user', async () => {
 // Delete user account, no auth token
 test('Should not delete account for unauthenticated user', async () => {
   await request(app)
-    .delete('/users/me')
+    .delete('/users/me') // delete from DB
     .send()
-    .expect(401)
+    .expect(401) // Assert res.status
 })
