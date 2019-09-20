8c74891b34cd3dc7d9bb4cbdec4e09f931db90cc
chrisbrinkmann <brinkmcd@gmail.com>
2019-09-19 23:32:45 -0400

    add --runInBand to npm test script to make test files run independently

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,4 +1,4 @@
-const require('supertest')
+const request = require('supertest')
 const app = require('../src/app')
 const Task = require('../src/models/task')
 const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
