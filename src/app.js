bc4048cb8c14bb7dd89eb72852a4d1148691a759
chrisbrinkmann <brinkmcd@gmail.com>
2019-09-22 16:56:15 -0400

    removed client frontend from project

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+const cors = require('cors')
 
 /**
  * Create the express server, mount middleware, routers, export.
@@ -9,6 +10,7 @@ const taskRouter = require('./routers/task')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json()) // auto parse req json to object
 app.use(userRouter) // register routers with app
 app.use(taskRouter)
