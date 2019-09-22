f97a4b91113c5cb14d5ea6f9ae0902ddbfaa70a4
chrisbrinkmann <brinkmcd@gmail.com>
2019-09-22 17:00:52 -0400

    added cookie parser and middleware to server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const app = require('./app')
+const cookieParser = require('cookie-parser')
 const port = process.env.PORT
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false })) // parses data sent via forms from frontend
+app.use(cookieParser) // parses cookies sent with forms
 
 /**
  * Start server
  */
-
 app.listen(port, () => {
   console.log('Server is listening on port: ' + port)
 })
